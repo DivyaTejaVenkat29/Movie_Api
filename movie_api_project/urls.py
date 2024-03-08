6ff07c09d229dd2b71c00a6785b1dc77a91e9abf
DivyaTejaVenk <divyatejavenkatk@gmail.com>
2024-03-08 18:41:25 +0530

    movie

--- movie_api_project/urls.py
+++ movie_api_project/urls.py
@@ -23,5 +23,3 @@ urlpatterns = [
     path('api/movies/',views.MovieListCreate.as_view()),
     path('api/movies/<int:pk>/',views.MovieRetrieveUpdateDestroy.as_view()),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
